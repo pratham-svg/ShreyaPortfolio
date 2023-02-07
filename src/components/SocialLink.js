27e0eb6c9e0340a678e115d77c59428f99981fd2
pratham-svg <prathampanchariya@gmail.com>
2023-02-08 01:40:11 +0530

    error fixed

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,3 +1,4 @@
+/* eslint-disable */ 
 import React from 'react'
 import { FaGithub , FaLinkedin } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
@@ -35,7 +36,7 @@ const SocialLink = () => {
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
         { link.map(({ id , child , href , style , download})=><li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " + style }>
-            <a href={href} className='flex justify-between itmes-center
+            <a href={href} className='flex justify-between items-center
              w-full text-white'
              download={download}
              target="_black"  
