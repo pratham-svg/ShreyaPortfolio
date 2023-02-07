8279277db5569fe61f19daef570387deacd6ef1d
pratham-svg <prathampanchariya@gmail.com>
2023-02-08 01:59:10 +0530

    repo updtaed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
     ]
   return (
     <div className=' flex justify-between items-center w-full
-    h-20 px-4 text-white bg-black fixed'>
+    h-20 px-4 text-white bg-black'>
         <div className='text-5xl font-signature'>
             Pratham
         </div>
