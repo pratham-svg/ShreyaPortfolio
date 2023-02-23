9aebfd14637abee35b852bae77f342e5b534fb54
pratham-svg <prathampanchariya@gmail.com>
2023-02-24 03:09:53 +0530

    about updated

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800
+    <div name='about' className='w-full h-full bg-gradient-to-b from-gray-800
     to-black text-white'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col
       justify-center w-full h-full'>
-        <div className='pd-8 py-3'>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'> About</p>
-        </div>
+        <div className='pd-8 py-3 my-20'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500 '> About</p>
+        
         <p className='text-xl mt-20'> Hello and welcome! My name is Pratham Pancahriya, and I am a passionate backend developer with a background in computer science.
         </p>
         <br/>
@@ -21,6 +21,7 @@ const About = () => {
         <br/> <br/>In my free time, I enjoy exploring new technologies, reading books, and staying active.
         <br/> <br/>Thank you for taking the time to read my bio, and I look forward to potentially working with you!
         </p>
+        </div>
       </div>
     </div>
   )
